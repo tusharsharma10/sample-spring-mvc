8da9ab69dd1c3366eb88421540f7f64336f8560e
lenovo <sharmatushar759@gmail.com>
2020-03-22 12:32:02 +0530

    ModelMap

--- src/main/java/learn/springmvc/LoginController.java
+++ src/main/java/learn/springmvc/LoginController.java
@@ -1,8 +1,10 @@
 package learn.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,7 +12,15 @@ public class LoginController {
 
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	
-	public String sayHello(){
+	public String showLoginPage(){
 		return "login";
+	}
+	
+@RequestMapping(value="/login", method = RequestMethod.POST)
+	
+	public String redirectToWelcomePage(@RequestParam String name,ModelMap map){
+	// key of map should match with value in name attribute in jsp file	
+	map.put("name", name);
+		return "welcome";
 	}
 }
